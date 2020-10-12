d32a7fb2787039926ee3a0802c536d256ef69c03
yuka0662 <rannmaru.865@gmail.com>
2020-10-12 22:59:00 +0900

    header,footerの作成、変更

---
+++ resources/js/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <p>copyright@farmteer all rights reserved.</p>
+    )
+}
+
+export default Footer;
---
+++ resources/js/components/Register.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Register() {
+    return <h1>新規作成</h1>;
+}
+
+export default Register;
